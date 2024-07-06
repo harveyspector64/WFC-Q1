9f7a14f281e263b983fce2b8708c7c9592ee8415
harveyspector64 <154754949+harveyspector64@users.noreply.github.com>
2024-07-05 20:36:45 -0700

    Update constants.js

--- scripts/utils/constants.js
+++ scripts/utils/constants.js
@@ -1,4 +1,4 @@
 // constants.js
 export const TILE_SIZE = 32;
-export const MAP_WIDTH = 20;
-export const MAP_HEIGHT = 20;
+export const MAP_WIDTH = 50;  // Increased width
+export const MAP_HEIGHT = 50; // Increased height
