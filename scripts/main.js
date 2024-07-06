9ef3c91d5627282377882a19a649ea3a880a4b0e
harveyspector64 <154754949+harveyspector64@users.noreply.github.com>
2024-07-06 09:01:06 -0700

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,6 @@
 // main.js
 import { loadTiles } from './tileLoader.js';
-import { generateWFCTile } from './mapGenerator.js';
+import { generateMap } from './mapGenerator.js';
 import { TILE_SIZE, MAP_WIDTH, MAP_HEIGHT } from './utils/constants.js';
 
 const canvas = document.getElementById('mapCanvas');
@@ -13,7 +13,7 @@ canvas.height = MAP_HEIGHT * TILE_SIZE;
 // Load tiles and generate the map
 loadTiles().then(() => {
     console.log('All tiles loaded');
-    generateWFCTile(ctx);
+    generateMap(ctx);
 }).catch(err => {
     console.error('Failed to load tiles:', err);
 });
