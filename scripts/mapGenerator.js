8697e76a8454e42b18fa3d0c39df798207cd18d3
harveyspector64 <154754949+harveyspector64@users.noreply.github.com>
2024-07-06 12:45:55 -0700

    Update mapGenerator.js

--- scripts/mapGenerator.js
+++ scripts/mapGenerator.js
@@ -1,6 +1,6 @@
 import { getTile } from './tileLoader.js';
 import { TILE_SIZE, MAP_WIDTH, MAP_HEIGHT } from './utils/constants.js';
-import { aStar, generateRoadNetwork } from './utils/aStar.js';
+import { aStar, createCurvedPath, generateRoadNetwork } from './utils/aStar.js';
 
 // Define more detailed patterns for different terrains
 const terrainPatterns = {
